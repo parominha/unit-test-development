c70fb0b1a5a52c941e609066da76f9936a7c398e
PALOMA AZEVEDO SANTOS <paloma.asantos17@senacsp.edu.br>
2022-10-10 18:06:41 -0300

    Adicionando teste para double

--- tests/Sample.Test/Extensions/DoubleExtensionsTest.cs
+++ tests/Sample.Test/Extensions/DoubleExtensionsTest.cs
@@ -1,11 +1,45 @@
-﻿using System;
+﻿using Sample.Extensions;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using Xunit;
 
 namespace Sample.Test.Extensions
 {
     public class DoubleExtensionsTest
     {
+        [Theory]
+        [InlineData(200.00)]
+        [InlineData(1500.00)]
+        [InlineData(10000.00)]
+        public void Should_ToStringMoneyPtBR_ReturnSucess(double value)
+        {
+            //arrange
+            string valueC;
+
+            //act
+            valueC = value.ToStringMoneyPtBR();
+
+            //assert
+            Assert.Contains("R$", valueC);
+        }
+
+
+
+        [Theory]
+        [InlineData(200.00)]
+        [InlineData(1500.00)]
+        [InlineData(10000.00)]
+        public void Should_ToStringMoneyPtBR_ReturnFalse(double value)
+        {
+            //arrange
+
+            //act
+
+            //assert
+
+        }
+
 
     }
 }
